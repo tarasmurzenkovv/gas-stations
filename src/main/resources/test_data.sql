f29649fe8a8b9649073c37ed23c0a9111dbbbf08
terance <murzenkovtn@gmail.com>
2016-07-16 16:35:10 +0300

    asdfa

--- src/main/resources/test_data.sql
+++ src/main/resources/test_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO "customer_type" ("id", "type_name") VALUES ("1", "REGULAR");
-INSERT INTO "customer_type" ("id", "type_name") VALUES ("2", "BUSINESS");
+INSERT INTO customer_type (id, type_name) VALUES (1, REGULAR);
+INSERT INTO customer_type (id, type_name) VALUES (2, BUSINESS);
 
