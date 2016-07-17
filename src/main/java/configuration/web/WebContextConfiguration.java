527a4ca62592b04a91c2100b0ff6ff2d282149b3
terance <murzenkovtn@gmail.com>
2016-07-17 19:40:59 +0300

    added swagger rest documentation

--- src/main/java/configuration/web/WebContextConfiguration.java
+++ src/main/java/configuration/web/WebContextConfiguration.java
@@ -24,8 +24,11 @@ public class WebContextConfiguration extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/*").addResourceLocations("/resources/");
 
-        registry.addResourceHandler("**/**").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Override
