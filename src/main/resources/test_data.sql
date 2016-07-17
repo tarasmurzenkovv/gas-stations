919b72e7fa0ca86345aa08f71af111a057b46620
terance <murzenkovtn@gmail.com>
2016-07-17 11:46:08 +0300

    reverted

--- src/main/resources/test_data.sql
+++ src/main/resources/test_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO "customer_type" ("id", "type_name") VALUES ("1", "REGULAR");
-INSERT INTO "customer_type" ("id", "type_name") VALUES ("2", "BUSINESS");
+INSERT INTO "d7aslo9caothtd".public."customer_type" ("id", "type_name") VALUES ("1", "REGULAR");
+INSERT INTO "d7aslo9caothtd".public."customer_type"  ("id", "type_name") VALUES ("2", "BUSINESS");
