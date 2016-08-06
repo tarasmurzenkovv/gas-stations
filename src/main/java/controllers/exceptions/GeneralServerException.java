a1559660f5f2c42bdbacaa9094e5f127e02f17a7
terance <murzenkovtn@gmail.com>
2016-08-06 16:37:41 +0300

    refactoring

--- src/main/java/controllers/exceptions/GeneralServerException.java
+++
@@ -1,7 +0,0 @@
-package controllers.exceptions;
-
-public class GeneralServerException extends RuntimeException {
-    public GeneralServerException(String message) {
-        super(message);
-    }
-}
